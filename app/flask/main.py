680411b1f2933ee80bdc9e8845209b29871f432e
Samuel Santos Guzman <samuel.santos@cpavs.mx>
2024-11-29 11:39:47 -0600

    Change: Update path for app errors log

--- app/flask/main.py
+++ app/flask/main.py
@@ -9,7 +9,7 @@ from PyPDF2 import PdfWriter
 
 
 logging.basicConfig(
-    filename='/app/logs/app-errors.log',
+    filename='/app/logs/errors.log',
     level=logging.ERROR,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
